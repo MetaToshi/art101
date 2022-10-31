49312e6fb08daf54fde080fe7ed5171e7970c35b
MetaToshi <MetaToshi@users.noreply.github.com>
2022-10-31 12:53:41 -0700

    Lab 9 - Javascript for the Web
    html DOM attributes/methods/elements practice

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -23,3 +23,20 @@ var result = num.map(function(x){
   return x ** 4;
 })
 console.log("Power of 4 of the array:", result);
+
+var outputEl = document.getElementById("output");
+outputEl.innerHTML = result; // put results here
+
+// timer callback
+const message = function() {
+    console.log("This message is shown after 5 seconds");
+}
+
+setTimeout(message, 5000);
+
+// question 3
+function callbackFunction(y) {
+  return y + " This is a string!"
+}
+
+console.log("Callback:", callbackFunction(result));
