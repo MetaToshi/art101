5f8b0b8d870d13908800c982a9604eaa14642f4a
MetaToshi <MetaToshi@users.noreply.github.com>
2022-11-09 14:19:47 -0800

    re: Lab 12
    Added class descriptions

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -9,18 +9,32 @@ Class: ART101
 function sortingHat(str) {
   // counts the letters in str and assigns it to a variable lendth
   var length_string = str.length % 4;
-  //
+
   if (length_string == 0) {
-    return "Gryffindor";
+    return "Gryffindor - Gryffindor house is where you would find the pluckiest \
+    and most daring students (there's a reason the house symbol is the brave lion).\
+    The house colours are scarlet and gold, the common room lies up in Gryffindor \
+    Tower and the Head of House is Professor Minerva McGonagall.";
   }
   else if (length_string == 1) {
-    return "Hufflepuff";
+    return "Hufflepuff - they value hard work, patience, loyalty, and fair play.\
+     The house has produced its share of great wizards - not least Newt Scamander,\
+     author of Fantastic Beasts and Where to find Them.";
   }
   else if (length_string == 2) {
-    return "Ravenclaw";
+    return "Ravenclaw - Ravenclaws possess the traits of cleverness, wisdom, wit,\
+     intellectual ability and creativity. According to Slytherin prefect Gemma\
+     Farley, Ravenclaws are so competitive when it comes to academic success that\
+     they are known to back stab each other, and likely other students, in order \
+     to get top marks.";
+
   }
   else if (length_string == 3) {
-    return "Slytherin";
+    return "Slytherin: Each house has a set of traits and characteristics \
+    associated with it, and those in Slytherin are known for being ambitious,\
+    cunning, and resourceful. Slytherins are also sometimes regarded as being\
+    evil thanks to the fact that many of the most sinister witches and wizards \
+    have been associated with this house.";
   }
 }
 
